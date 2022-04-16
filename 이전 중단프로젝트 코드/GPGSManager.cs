a7d999cbb961b1c7995d6990a19f93aa228ed03d
EZHoon93 <47181408+EZHoon93@users.noreply.github.com>
2022-04-16 17:10:15 +0900

    추가

---
+++ 이전 중단프로젝트 코드/GPGSManager.cs
@@ -0,0 +1,131 @@
+﻿
+using UnityEngine;
+using TMPro;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using GooglePlayGames.BasicApi.SavedGame;
+public static class GPGSManager 
+{
+
+    public static void Login()
+    {
+        var config = new PlayGamesClientConfiguration.Builder().EnableSavedGames().Build();
+        PlayGamesPlatform.InitializeInstance(config);
+        PlayGamesPlatform.DebugLogEnabled = true;
+        PlayGamesPlatform.Activate();
+        Social.localUser.Authenticate((bool success) =>
+        {
+            if (success)
+            {
+                LoadCloud();
+            }
+            else
+            {
+                
+            }
+        });
+
+    }
+
+    #region 클라우드 저장
+
+    /// <summary>
+    /// 긴
+    /// </summary>
+    /// <returns></returns>
+
+    public static void LoadCloud()
+    {
+        PlayGamesPlatform.Instance.SavedGame.OpenWithAutomaticConflictResolution("mysave", DataSource.ReadCacheOrNetwork, ConflictResolutionStrategy.UseLastKnownGood, LoadGame);
+    }
+
+    static void LoadGame(SavedGameRequestStatus status, ISavedGameMetadata game)
+    {
+        if (status == SavedGameRequestStatus.Success)
+        {
+            PlayGamesPlatform.Instance.SavedGame.ReadBinaryData(game, LoadData);
+
+        }
+    }
+
+    static void LoadData(SavedGameRequestStatus status, byte[] LoadedData)
+    {
+        if (status == SavedGameRequestStatus.Success)
+        {
+            string data = System.Text.Encoding.UTF8.GetString(LoadedData);
+            var userData = JsonUtility.FromJson<UserData>(data);
+
+            if (userData == null)
+            {
+                //loginManager.SetActiveCreateNickName(true);
+            }
+            else
+            {
+                //loginManager.SetActiveCreateNickName(false);
+            }
+
+        }
+        else
+        {
+
+
+        }
+
+    }
+
+
+
+    public static void SaveCloud()
+    {
+        PlayGamesPlatform.Instance.SavedGame.OpenWithAutomaticConflictResolution("mysave",
+            DataSource.ReadCacheOrNetwork, ConflictResolutionStrategy.UseLastKnownGood, SaveGame);
+    }
+
+    static void SaveGame(SavedGameRequestStatus status, ISavedGameMetadata game)
+    {
+        if (status == SavedGameRequestStatus.Success)
+        {
+            var update = new SavedGameMetadataUpdate.Builder().Build();
+            var userData = PlayerInfo.userData;
+            string jsonData = JsonUtility.ToJson(userData, true);
+            byte[] bytes = System.Text.Encoding.UTF8.GetBytes(jsonData);
+
+
+            PlayGamesPlatform.Instance.SavedGame.CommitUpdate(game, update, bytes, SaveData);
+        }
+    }
+
+    static void SaveData(SavedGameRequestStatus status, ISavedGameMetadata game)
+    {
+        if (status == SavedGameRequestStatus.Success)
+        {
+            
+        }
+        else
+        {
+
+        }
+    }
+
+
+
+    public static void DeleteCloud()
+    {
+        PlayGamesPlatform.Instance.SavedGame.OpenWithAutomaticConflictResolution("mysave",
+            DataSource.ReadCacheOrNetwork, ConflictResolutionStrategy.UseLongestPlaytime, DeleteGame);
+    }
+
+    static void DeleteGame(SavedGameRequestStatus status, ISavedGameMetadata game)
+    {
+        if (status == SavedGameRequestStatus.Success)
+        {
+            PlayGamesPlatform.Instance.SavedGame.Delete(game);
+        }
+        else
+        {
+
+        }
+    }
+
+    #endregion
+}
---
+++ 이전 중단프로젝트 코드/GoogleSheetManager.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+[System.Serializable]
+public class serverDataEtc
+{
+    public string key;
+    public string value;
+}
+
+
+[System.Serializable]
+public class GoogleData
+{
+    public List<serverCharacterData> dataCharacter;
+    public List<serverProjectileData> dataProjectile;
+    public List<serverDataEtc> dataEtc;
+}
+public class GoogleSheetManager : MonoBehaviour
+{
+    public GoogleData GD;
+    const string URL = "https://script.google.com/macros/s/AKfycbyhAgHCJuntujgoQS2tD7WhJ5avJ7FmUJlDEAL6sB60fTbEoSNrhELjEaU-YQKIiSaF/exec";
+
+
+    public bool isDataOk;
+    void SetData()
+    {
+        //캐릭터 데이터 셋업
+        foreach (var c in GD.dataCharacter)
+        {
+            var characterScriptableData = DataEtc.Instance.GetCharacterDataByServerKey(c.key);
+            characterScriptableData.SetupProductData(c);
+        }
+
+        //무기 데이터 셋업
+        foreach (var p in GD.dataProjectile)
+        {
+            var projectileScriptableData = DataEtc.Instance.GetProjectileDataByServerKey(p.key);
+            projectileScriptableData.SetupData(p);
+
+        }
+
+        foreach (var e in GD.dataEtc)
+        {
+            var etcScriptableData = DataEtc.Instance.GetEtcDataByServerKey(e.key);
+            etcScriptableData.SetupData(e);
+        }
+
+        AbilitySkill_HardCoding();
+    }
+    
+
+    /// <summary>
+    /// 스킬 데이터 강제로 하드코딩해서 /.
+    /// </summary>
+    void AbilitySkill_HardCoding()
+    {
+
+        var run = DataContainer.Instance.GetAbilityByType(AbilityType.AP_r) as SkillContainer;
+        var dark = DataContainer.Instance.GetAbilityByType(AbilityType.AP_d) as SkillContainer;
+        var heal = DataContainer.Instance.GetAbilityByType(AbilityType.AP_h) as SkillContainer;
+        var shield = DataContainer.Instance.GetAbilityByType(AbilityType.AP_s) as SkillContainer;
+
+        run.SetUpHardCoding(DataEtc.Instance.sRunCoolTime, DataEtc.Instance.sRunDurationTime);
+        dark.SetUpHardCoding(DataEtc.Instance.sDarkCoolTime, DataEtc.Instance.sDarkDurationTime);
+        heal.SetUpHardCoding(DataEtc.Instance.sHealCoolTime, 0);
+        shield.SetUpHardCoding(DataEtc.Instance.sShieldCoolTime, DataEtc.Instance.sShieldDurationTime);
+
+
+
+
+
+
+    }
+
+
+
+
+    public void GetServerData()
+    {
+        isDataOk = false;
+        WWWForm form = new WWWForm();
+        form.AddField("order", "data");
+        StartCoroutine(Post(form));
+    }
+
+
+    IEnumerator Post(WWWForm form)
+    {
+        using (UnityWebRequest www = UnityWebRequest.Post(URL, form)) // 반드시 using을 써야한다
+        {
+            yield return www.SendWebRequest();
+            if (www.isDone) Response(www.downloadHandler.text);
+            else print("웹의 응답이 없습니다.");
+        }
+    }
+
+    void Response(string json)
+    {
+        isDataOk = true;
+        GD = JsonUtility.FromJson<GoogleData>(json);    //Json으로 파싱
+        SetData(); //데이터 세팅
+    }
+
+
+}
